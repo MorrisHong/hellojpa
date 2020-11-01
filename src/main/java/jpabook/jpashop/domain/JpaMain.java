4c240bcc088d0e5cd76096d535a6ae74f3373a05
MorrisHong <govlmo91@gmail.com>
2020-11-01 12:39:15 +0900

    객체지향이 아닌 rdb에 맞춘 설계

---
+++ src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * @author : Eunmo Hong
+ * @since : 2020/11/01
+ */
+
+public class JpaMain {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}finally {
+			em.close();
+		}
+
+		emf.close();
+	}
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.domain;
+
+/**
+ * @author : Eunmo Hong
+ * @since : 2020/11/01
+ */
+
+public enum OrderStatus {
+	ORDER,CANCEL
+}
